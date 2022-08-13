b5cd12f26e9ec5f5cbc9641867ba20cb3196f4f9
arun-gogox <arun.kumar@gogox.com>
2022-08-13 15:05:16 +0800

    new-commit

---
+++ src/main/java/com/example/test/topic/TopicService.java
@@ -0,0 +1,43 @@
+package com.example.test.topic;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class TopicService {
+    private final List<Topic> topics = new ArrayList<>(Arrays.asList(
+            new Topic("Spring", "Spring Framework", "Spring Framework Description"),
+            new Topic("Java", "Core Java Framework", "Java Framework Description"),
+            new Topic("JavaScript", "JavaScript", "JavaScript Framework Description")
+    ));
+
+
+    public List<Topic> getAllTopics() {
+        return topics;
+    }
+
+    public Topic getTopic(String id) {
+        return topics.stream().filter(topic -> topic.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
+    }
+
+    public void deleteTopic(String id) {
+        topics.removeIf(topic -> topic.getId().equals(id));
+    }
+
+    public void updateTopic(Topic topic, String id) {
+        for (int i = 0; i < topics.size(); i++) {
+            Topic t = topics.get(i);
+            if (t.getId().equals(id)) {
+                topics.set(i, topic);
+                return;
+            }
+        }
+    }
+}
